97248f6e0ebd4347be35ad3aef92d7fe73cf2c9d
朱合健 <zhuhejian@kdniao.com>
2019-03-25 16:10:05 +0800

    部署vue打包文件

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
