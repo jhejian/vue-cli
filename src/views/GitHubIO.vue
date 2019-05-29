8d016ef15c6545fc2aeb4f97979280dd543b684f
朱合健 <zhuhejian@kdniao.com>
2019-05-29 11:01:53 +0800

    add GitHubIO.vue

---
+++ src/views/GitHubIO.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <pre>
+        vue项目部署gitHubgithub服务器：
+            1.创建一个仓库，名称格式为username.github.io，
+            其中username为用户名
+
+            2.克隆仓库到本地
+
+            3.将vue打包文件static和index.html放置于克隆
+            出来的文件夹下，然后提交到GitHub
+
+            4.浏览器输入http://username.github.io预览
+        </pre>
+        <a href="https://jhejian.github.io">https://jhejian.github.io</a>
+    </div>
+</template>
